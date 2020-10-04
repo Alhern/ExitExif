a07e79ff93019465456995e6a558c5f3474afbda
Alhern <23422910+Alhern@users.noreply.github.com>
2020-10-04 20:31:24 +0200

    fetching EXIF data and displaying it

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PIL import Image
+from PIL import Image, ExifTags
 
 #def remove_data()
 
@@ -11,6 +11,15 @@ def main():
 		try:
 			with Image.open(files) as img:
 				print("Filename: %s\nFormat: %s\nSize: %s\nMode: %s" % (files, img.format, img.size, img.mode))
+				data = img._getexif()
+				if (data == None):
+					print("No EXIF data.")
+				else:
+					print("Raw EXIF:\n%s\n" % data)
+					print("Decoded EXIF:")
+					for key, val in data.items():
+						if key in ExifTags.TAGS:
+							print("%s: %s" % (ExifTags.TAGS[key], repr(val)))
 		except OSError:
 			pass
 
