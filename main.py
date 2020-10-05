8b5fd357bce7f536e58921ab66391438431bf8e8
Alhern <23422910+Alhern@users.noreply.github.com>
2020-10-05 17:23:56 +0200

    added user input

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from PIL import Image, ExifTags
 #def remove_data()
 
 def main():
-	for files in sys.argv[1:]:
-		try:
-			with Image.open(files) as img:
-				print("Filename: %s\nFormat: %s\nSize: %s\nMode: %s" % (files, img.format, img.size, img.mode))
-				data = img._getexif()
-				if (data == None):
-					print("No EXIF data.")
-				else:
-					print("Raw EXIF:\n%s\n" % data)
-					print("Decoded EXIF:")
-					for key, val in data.items():
-						if key in ExifTags.TAGS:
-							print("%s: %s" % (ExifTags.TAGS[key], repr(val)))
-		except OSError:
-			pass
+	f = input("Please enter your image's filename: ")
+	try:
+		with Image.open(f) as img:
+			print("Filename: %s\nFormat: %s\nSize: %s\nMode: %s" % (f, img.format, img.size, img.mode))
+			data = img._getexif()
+			if (data == None):
+				print("No EXIF data.")
+			else:
+				print("Raw EXIF:\n%s\n" % data)
+				print("Decoded EXIF:")
+				for key, val in data.items():
+					if key in ExifTags.TAGS:
+						print("%s: %s" % (ExifTags.TAGS[key], repr(val)))
+	except OSError:
+		pass
 
 
 if __name__ == "__main__": main()
